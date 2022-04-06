2d5f11be1f7d70bc16be2e9cff0439e92d55c9b9
sudharsanmaran <smsudharsanmaran>
2022-04-06 17:30:47 +0530

    initial commit

---
+++ gkg.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup as bs
+import requests
+
+
+def getsoup():
+    url='https://www.geeksforgeeks.org/python-programming-language/'
+    req_txt=requests.get(url)
+    return bs(req_txt.content,'html.parser')
+
+
+def getcontent(soup):
+    content=soup.find('div',class_='entry-content')
+    return content
+
+
+def printresult(content):
+    res = content.find_all('p')
+    for r in res:
+        r.text
+
+
+def scrapgkg():
+    soup=getsoup()
+    print(soup.prettify())
+    print("##################")
+    content = getcontent(soup)
+    print(content)
+    print("##################")
+    printresult(content)
+
+
+
+if __name__=='__main__':
+    scrapgkg()
